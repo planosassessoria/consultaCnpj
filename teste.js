f2209e6f9c35b21f767c2edbdb738e572d0db4a8
Cezar Pretto <cezar@planosassessoria.com.br>
2018-07-03 17:33:23 -0300

    Adicionando proxy

--- teste.js
+++ teste.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const run = async () => {
   try {
-    const params = await getParams()
+    const params = await getParams({ proxy: 'http://dorcilio@gmail.com:kmab1960@br.torguardvpnaccess.com:6060' })
     console.log(params)
   } catch (e) {
     console.error(e)
